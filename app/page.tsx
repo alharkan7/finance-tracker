4f8fa6c46e0541af5205fcf55c40a8e6b4b2b1aa
Al Harkan <ali.al81@ui.ac.id>
2025-02-16 09:53:46 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -211,7 +211,7 @@ export default function Component() {
             <Button
               type="button"
               className="w-1/2 bg-white shadow-sm hover:shadow-md transition-all duration-200 border border-gray-200 text-gray-700 hover:text-primary-foreground rounded-lg py-4 flex items-center justify-center gap-2 font-medium"
-              onClick={() => window.location.href = "https://bit.ly/adexpense-dash"}
+              onClick={() => window.location.href = "https://bit.ly/adexpense-dashboards"}
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="18" height="18" x="3" y="3" rx="2" /><path d="M21 12H3" /><path d="M12 3v18" /></svg>
               Dashboard
