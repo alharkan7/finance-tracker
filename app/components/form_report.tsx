b3797cd898e9d7dd5da2843a623fe715546b9176
alharkan7 <ali.al81@ui.ac.id>
2025-09-08 15:50:10 +0700

    Refactor calendar component to simplify icon rendering by removing unnecessary props from IconLeft and IconRight components.

--- app/components/form_report.tsx
+++ app/components/form_report.tsx
@@ -1,9 +1,8 @@
 'use client'
 
 import React, { useState, useEffect } from 'react';
-import { PieChart, Pie, Cell, ResponsiveContainer, Legend, Tooltip } from 'recharts';
+import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from 'recharts';
 import { Button } from "@/components/ui/button"
-import { Label } from "@/components/ui/label"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Calendar, RefreshCw } from 'lucide-react';
